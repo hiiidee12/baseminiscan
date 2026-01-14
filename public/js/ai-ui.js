05270e351cae17f9ddacc4deedd1e378bf9fc004
hiiidee12 <125862664+hiiidee12@users.noreply.github.com>
2026-01-14 18:28:25 +0700

    Update ai-ui.js

--- public/js/ai-ui.js
+++ public/js/ai-ui.js
@@ -172,6 +172,6 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   if (__aiMessages.length === 0) {
-    __aiAdd("assistant", "🤖:Welcome to BMS AI Assistant.");
+    __aiAdd("assistant", "🤖:Welcome to BMS AI assistant.");
   }
 });
