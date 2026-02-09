c43847bbf4854fbbeed6d7ebb3a13448eb5caafe
hiiidee12 <125862664+hiiidee12@users.noreply.github.com>
2026-02-09 19:34:49 +0700

    Increase transaction count page size to 2000

--- api/address.js
+++ api/address.js
@@ -270,7 +270,7 @@ export default async function handler(req, res) {
     // Optional: fetch approximate transaction count
     let txCount = null;
     if (wantCount && COUNT_KEYS.length) {
-      const PAGE = 500;
+      const PAGE = 2000;
 
       const pageLen = async (apikey, p) => {
         const url = `${API}?${qs({
